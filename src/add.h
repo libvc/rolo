058ef2389e232ee19dfbb9a1dfc8743564598736
Andrew Hsu <ahsu@users.sf.net>
2003-05-20 01:08:52 +0000

    Doc fixes.

--- src/add.h
+++ src/add.h
@@ -1,27 +1,27 @@
 /*
- * rolo - contact management software
- * Copyright (C) 2003  Andrew Hsu
+ *  rolo - contact management software
+ *  Copyright (C) 2003  Andrew Hsu
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307 USA
  *
- *
- * $Id: add.h,v 1.2 2003/03/26 11:18:40 ahsu Rel $
+ *  $Id$
  */
 
 enum add_entry_results
 { ADD_SUCCESSFUL, ADD_ABORTED };
 
-extern int add_entry (const char *datafile);
 extern void set_add_editor (const char *str);
+extern int add_entry (const char *datafile);
--- src/help.h
+++ src/help.h
@@ -1,28 +1,28 @@
 /*
- * rolo - contact management software
- * Copyright (C) 2003  Andrew Hsu
+ *  rolo - contact management software
+ *  Copyright (C) 2003  Andrew Hsu
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307 USA
  *
- *
- * $Id: help.h,v 1.1 2003/02/16 06:12:22 ahsu Rel $
+ *  $Id$
  */
 
 #include <ncurses.h>
 
-extern void init_help();
-extern void show_view_help();
-extern void show_edit_help();
-extern void show_index_help();
+extern void init_help ();
+extern void show_view_help ();
+extern void show_edit_help ();
+extern void show_index_help ();
