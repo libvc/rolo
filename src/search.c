5c172b7dab32db141e630f61c03b88be5acaf02a
Andrew Hsu <ahsu@users.sf.net>
2003-05-21 00:01:00 +0000

    Searching is now case-insensitive.

--- src/search.c
+++ src/search.c
@@ -17,23 +17,68 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  *  02111-1307 USA
  *
- *  $Id$
+ *  $Id: search.c,v 1.1 2003/05/20 04:59:33 ahsu Exp $
  */
 
 #include "search.h"
 #include <string.h>
+
+static int strstr_nocase (const char *haystack, const char *needle);
+
+/***************************************************************************
+    Searches for the needle in the haystack without worrying about
+    the case.  Returns 1 if found, 0 otherwise.
+ */
+
+static int
+strstr_nocase (const char *haystack, const char *needle)
+{
+  int result = 0;
+
+  if (NULL != haystack && NULL != needle)
+    {
+      char tmp_haystack[80];
+      char tmp_needle[80];
+      int len = 0;
+      int i = 0;
+
+      strncpy (tmp_haystack, haystack, sizeof (tmp_haystack) - 1);
+      tmp_haystack[sizeof (tmp_haystack) - 1] = '\0';
+      strncpy (tmp_needle, needle, sizeof (tmp_needle) - 1);
+      tmp_needle[sizeof (tmp_haystack) - 1] = '\0';
+
+      len = strlen (tmp_haystack);
+      for (i = 0; i < len; i++)
+        {
+          tmp_haystack[i] = tolower (tmp_haystack[i]);
+        }
+
+      len = strlen (tmp_needle);
+      for (i = 0; i < len; i++)
+        {
+          tmp_needle[i] = tolower (tmp_needle[i]);
+        }
+
+      if (NULL != strstr (tmp_haystack, tmp_needle))
+        {
+          result = 1;
+        }
+    }
+
+  return result;
+}
 
 /***************************************************************************
     Perform the search for an item with a given search string.
  */
 
 ITEM *
-search_menu (MENU *menu, const char *search_string)
+search_menu (MENU * menu, const char *search_string)
 {
   ITEM *result_entry = NULL;
   int i = 0;
   ITEM **items = NULL;
-  char *found_string = NULL;
+  int found_string = 0;
   bool done = FALSE;
 
   if (NULL != search_string)
@@ -43,9 +88,10 @@ search_menu (MENU *menu, const char *search_string)
 
       while (!done && (items[i] != NULL))
         {
-          found_string = strstr (item_description (items[i]), search_string);
+          found_string =
+              strstr_nocase (item_description (items[i]), search_string);
 
-          if (found_string != NULL)
+          if (found_string == 1)
             {
               result_entry = items[i];
               done = TRUE;
@@ -57,4 +103,3 @@ search_menu (MENU *menu, const char *search_string)
 
   return result_entry;
 }
-
